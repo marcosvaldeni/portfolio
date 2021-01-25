577fe1ff0e6dba534a08816d3e956380674e4439
Marcos Lucas <marcosvaldeni@gmail.com>
2021-01-25 22:25:57 +0000

    about component

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
+import svgCode from '../../img/code.svg';
+import svgDesktop from '../../img/desktop.svg';
+import svgYoutube from '../../img/youtube.svg';
+
+import { Container, Section } from './styles';
+
 const About = () => {
 
   return (
-    <h1>About</h1>
+    <>
+      <Container>
+        <h2>ABOUT ME</h2>
+        <img 
+          src="https://avatars.githubusercontent.com/u/15179803?s=460&u=6c2d2619c59abf5748c38642ab637cc33a36fcb8&v=4" 
+          alt="Profile Photo"
+        />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga 
+          sapiente ipsum vitae nulla aut inventore repellendus qui. Porro.
+          Fuga sapiente ipsum vitae nulla aut inventore repellendus qui. Porro.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur 
+          id architecto aperiam modi, quo iure repudiandae in rerum, ea 
+          consequatur magnam facere pariatur!Lorem ipsum dolor sit amet, 
+          consectetur adipisicing elit. Fuga sapiente ipsum vitae nulla aut 
+          inventore repellendus qui. Porro.
+        </p>
+
+        <Section>
+          <h3>SELF PROCLAMATIONS:</h3>
+          <ul>
+            <li>
+              <img src={svgCode} alt="icon"/>
+              <span>Frontend Dev</span>
+            </li>
+            <li>
+              <img src={svgDesktop} alt="icon"/>
+              <span>UX Enthusiast</span>
+            </li>
+            <li>
+              <img src={svgYoutube} alt="icon"/>
+              <span>Youtuber Apprentice</span>
+            </li>
+          </ul>
+        </Section>
+      </Container>
+    </>
+
+    
   );
 }
 
--- src/components/about/styles.js
+++ src/components/about/styles.js
@@ -1,5 +1,89 @@
 import styled from 'styled-components';
 
-export const container = styled.div`
+export const Container = styled.div`
+  min-height: 100vh;
+  width: 100%;
+  padding: 40px 20px 0 20px;
+  background: #F2F2F2;
+  text-align: center;
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-size: 16px;
+    font-weight: 300;
+    letter-spacing: 4px;
+    margin-bottom: 20px
+  }
+
+  > img {
+    height: 80px;
+    border-radius: 50%;
+    margin-bottom: 30px;
+  }
+
+  p {
+    max-width: 760px;
+    font-size: 16px;
+    line-height: 26px;
+  }
 `;
+
+export const Section = styled.div`
+  width: 100%;
+  max-width: 760px;
+  margin-top: 30px;
+  margin-bottom: 60px;
+  padding: 35px 50px;
+  background: #fff;
+  border-radius: 30px;
+
+  h3 {
+    font-size: 12px;
+    font-weight: 300;
+    letter-spacing: 4px;
+    margin-bottom: 40px;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  li {
+    font-size: 14px;
+    font-weight: 300;
+
+    flex: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      height: 50px;
+    }
+
+    span {
+      display: block;
+    }
+
+    & + li {
+      margin-top: 32px;
+    }
+  }
+
+  @media (min-width: 576px) {
+    ul {
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        margin-top: 0;
+      }
+
+    }
+
+  }
+`;
+
